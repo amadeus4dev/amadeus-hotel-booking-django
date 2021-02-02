c29c5c6cf0bf84c5fd391ecf60ed373fb19217af
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-02-02 16:22:35 +0100

    update booking method

--- amadeus_demo_api/demo/views.py
+++ amadeus_demo_api/demo/views.py
@@ -43,6 +43,42 @@ def demo(request):
     return render(request, 'demo/demo_form.html', {})
 
 
+def rooms_per_hotel(request, hotel, departureDate, returnDate):
+    try:
+        # Search for rooms in a given hotel
+        rooms = amadeus.shopping.hotel_offers_by_hotel.get(hotelId=hotel,
+                                                           checkInDate=departureDate,
+                                                           checkOutDate=returnDate).data
+
+        hotel_rooms = Room(rooms).construct_room()
+        return render(request, 'demo/rooms_per_hotel.html', {'response': hotel_rooms,
+                                                             'name': rooms['hotel']['name'],
+                                                             })
+    except (TypeError, AttributeError, ResponseError, KeyError) as error:
+        messages.add_message(request, messages.ERROR, error)
+        return render(request, 'demo/rooms_per_hotel.html', {})
+
+
+def book_hotel(request, offer_id):
+    try:
+        offer_availability = amadeus.shopping.hotel_offer(offer_id).get()
+        if offer_availability.status_code == 200:
+            guests = [{'id': 1, 'name': {'title': 'MR', 'firstName': 'BOB', 'lastName': 'SMITH'},
+                   'contact': {'phone': '+33679278416', 'email': 'bob.smith@email.com'}}]
+
+            payments = {'id': 1, 'method': 'creditCard',
+                    'card': {'vendorCode': 'VI', 'cardNumber': '4151289722471370', 'expiryDate': '2021-08'}}
+            booking = amadeus.booking.hotel_bookings.post(offer_id, guests, payments).data
+        else:
+            return render(request, 'demo/booking.html', {'response': 'The room is not available'})    
+    except ResponseError as error:
+        messages.add_message(request, messages.ERROR, error.response.body)
+        return render(request, 'demo/booking.html', {})    
+    return render(request, 'demo/booking.html', {'id': booking[0]['id'],
+                                             'providerConfirmationId': booking[0]['providerConfirmationId']
+                                             })
+
+
 def city_search(request):
     if request.is_ajax():
         try:
@@ -58,38 +94,4 @@ def get_city_list(data):
     for i, val in enumerate(data):
         result.append(data[i]['iataCode'] + ', ' + data[i]['name'])
     result = list(dict.fromkeys(result))
-    return json.dumps(result)
-
-
-def book_hotel(request, offer_id):
-    try:
-        # Confirm availability and price
-        amadeus.shopping.hotel_offer(offer_id).get()
-        guests = [{'id': 1, 'name': {'title': 'MR', 'firstName': 'BOB', 'lastName': 'SMITH'},
-                   'contact': {'phone': '+33679278416', 'email': 'bob.smith@email.com'}}]
-
-        payments = {'id': 1, 'method': 'creditCard',
-                    'card': {'vendorCode': 'VI', 'cardNumber': '4151289722471370', 'expiryDate': '2021-08'}}
-        booking = amadeus.booking.hotel_bookings.post(offer_id, guests, payments).data
-    except ResponseError as error:
-        messages.add_message(request, messages.ERROR, error.response.body)
-        return render(request, 'demo/booking.html', {})
-    return render(request, 'demo/booking.html', {'id': booking[0]['id'],
-                                                 'providerConfirmationId': booking[0]['providerConfirmationId']
-                                                 })
-
-
-def rooms_per_hotel(request, hotel, departureDate, returnDate):
-    try:
-        # Search for rooms in a given hotel
-        rooms = amadeus.shopping.hotel_offers_by_hotel.get(hotelId=hotel,
-                                                           checkInDate=departureDate,
-                                                           checkOutDate=returnDate).data
-
-        hotel_rooms = Room(rooms).construct_room()
-        return render(request, 'demo/rooms_per_hotel.html', {'response': hotel_rooms,
-                                                             'name': rooms['hotel']['name'],
-                                                             })
-    except (TypeError, AttributeError, ResponseError, KeyError) as error:
-        messages.add_message(request, messages.ERROR, error)
-        return render(request, 'demo/rooms_per_hotel.html', {})
+    return json.dumps(result)        
